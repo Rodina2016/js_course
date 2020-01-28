658588ba7d90a50c16ed8b03a493d676943ecb27
Marodina <marusea_rod@list.ru>
2020-01-28 23:31:29 +0300

    fix

--- lesson4/addLesson4/js/script.js
+++ lesson4/addLesson4/js/script.js
@@ -4,16 +4,15 @@ let strTrim = function(str) {
 
     if(typeof(str) === 'string') {
         str = str.trim();
+        if(str.length > 30) {
+            str = str.slice(0,30) + "...";
+        }
     } else {
         console.log('Надо ввести строку');
-    }
-
-    if(str.length > 30) {
-        str = str.slice(0,30) + "...";
     }
 
     return str;
 }
 
-let newStr = strTrim('Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев');
+let newStr = strTrim(30);
 console.log(newStr);
