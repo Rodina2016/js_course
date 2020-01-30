fa1c1a35d3972b7d67fe36f6fe152e1893a23007
Masha <marusea_rod@list.ru>
2020-01-30 21:12:53 +0300

    add lesson5

---
+++ lesson5/mainLesson5/js/script.js
@@ -0,0 +1,88 @@
+var money,
+income = 'фриланс',
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
+mission = 150000;
+
+let expenses = [];
+let getExpensesAmount = getExpensesMonth();
+console.log('getExpensesAmount', getExpensesAmount);
+
+start();
+
+(function showTypeOf() {
+    console.log(typeof(money));
+    console.log(typeof(income));
+    console.log(typeof(deposit));
+}());
+
+let str = addExpenses.toLowerCase();
+addExpenses = str.split(',');
+console.log(addExpenses);
+
+let accumulatedMonth = getAccumulatedMonth();
+getTargetMonth();
+
+let budgetDay =  Math.floor(accumulatedMonth/30);
+
+console.log('budgetDay: ', budgetDay);
+
+if(budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay <= 600 && budgetDay > 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0) {
+    console.log('Что-то пошло не так');
+}
+
+getStatusIncome();
+
+function getTargetMonth() {
+    console.log('money', money);
+    let month =  Math.ceil(mission/(money - getExpensesAmount));
+    if(month > 0) {
+        console.log(`За ${month} месяцев вы достигните свое цели`);
+    } else {
+        console.log(`Цель не будет достигнута...`);
+    }
+}
+
+function getStatusIncome() {
+    console.log(income);
+}
+
+function getAccumulatedMonth() {
+    return money - getExpensesAmount;
+}
+
+function getExpensesMonth() {
+    let sum = 0;
+    let howMuch = 0;
+
+    for(let i =0; i < 2; i++) {
+        expenses[i] = prompt('Введите обязательную статью расходов?');
+        howMuch = prompt('Во сколько это обойдется?');
+        while(!letIsNumber(howMuch)) {
+            howMuch = prompt('Во сколько это обойдется?');
+        }
+        sum += +howMuch;
+
+    }
+
+    console.log(sum);
+    return sum;
+}
+
+function start() {
+    do {
+        money = prompt('Ваш месячный доход');
+    } while (!letIsNumber(money));
+
+
+}
+
+function letIsNumber(n) {
+    return !isNaN(parseFloat(n))  && isFinite(n);
+}
