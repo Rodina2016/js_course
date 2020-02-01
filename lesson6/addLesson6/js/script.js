2b959f5ca44f966d4d2b473d9fc1d2fb698206c5
Mariy <marodina87@gmail.com>
2020-02-01 17:13:55 +0300

    add addlesson6

--- lesson6/addLesson6/js/script.js
+++ lesson6/addLesson6/js/script.js
@@ -8,36 +8,37 @@ function bot(x, tryCount) {
         if(number === null){
             return;
         }else {
-            console.log(tryCount);
-            console.log(count);
-            if(tryCount >= count){
+            count++;
+            if(tryCount > count){
                 if(+number > x) {
                     count++;
                     alert('Загаданное число меньше, осталось ' + (tryCount - count) + 'попыток');
                     let start = bot(x,tryCount);
-                    start();
+                    start(count);
                 } else if (+number < x) {
-                    count++;
                     alert('Загаданное число больше, осталось ' + (tryCount - count) + 'попыток');
                     let start = bot(x,tryCount);
-                    start();
+                    start(count);
                 } else if(isNaN(+number)){
-                    count++;
                     alert('Введите число, осталось ' + (tryCount - count) + 'попыток');
                     let start = bot(x,tryCount);
-                    start();
+                    start(count);
                 } else if (+number === x) {
                     alert('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-                    start();
+                    start(0);
                 }
             } else {
-                alert('Попытки закончились, хотите сыграть еще?');
-                let start = bot(40,5);
-                start();
+                if(confirm('Попытки закончились, хотите сыграть еще?')){
+                    let start = bot(40,5);
+                    start(0);
+                }else {
+                    return;
+                }
+                
             }
         }
     }
 }
 
 let start = bot(40,5);
-start();
+start(0);
