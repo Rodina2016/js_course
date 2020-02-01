19b8fbf5e67eab9b04a9b71f56d39dbdc7e3170c
Mariy <marodina87@gmail.com>
2020-02-01 16:58:57 +0300

    add main lesson6

--- lesson6/addLesson6/js/script.js
+++ lesson6/addLesson6/js/script.js
@@ -1,26 +1,43 @@
+
 'use strict';
 
-let arr = ['5465464','2554','7687','45656', "255456"];
+function bot(x, tryCount) {
+    let number = prompt('Угадай число от 1 до 100');
 
-for( let i = 0; i < arr.length; i++) {
-    if(+arr[i][0] === 2 || +arr[i][0] === 4) {
-        console.log(arr[i]);
+    return function(count) {
+        if(number === null){
+            return;
+        }else {
+            console.log(tryCount);
+            console.log(count);
+            if(tryCount >= count){
+                if(+number > x) {
+                    count++;
+                    alert('Загаданное число меньше, осталось ' + (tryCount - count) + 'попыток');
+                    let start = bot(x,tryCount);
+                    start();
+                } else if (+number < x) {
+                    count++;
+                    alert('Загаданное число больше, осталось ' + (tryCount - count) + 'попыток');
+                    let start = bot(x,tryCount);
+                    start();
+                } else if(isNaN(+number)){
+                    count++;
+                    alert('Введите число, осталось ' + (tryCount - count) + 'попыток');
+                    let start = bot(x,tryCount);
+                    start();
+                } else if (+number === x) {
+                    alert('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                    start();
+                }
+            } else {
+                alert('Попытки закончились, хотите сыграть еще?');
+                let start = bot(40,5);
+                start();
+            }
+        }
     }
 }
 
-for(let i = 1; i < 100; i++) {
-    let k = 0;
-    for(let j = 2; j < 100; j++) {
-        let ost = i%j;
-        if(!ost) {
-            k++;
-        }
-        if(k > 1) {
-            break;
-        }
-    }
-    if(k === 1) {
-        console.log(`Делители этого числа: 1 и ${i}`);
-    }
-
-}
+let start = bot(40,5);
+start();
--- lesson6/mainLesson6/js/script.js
+++ lesson6/mainLesson6/js/script.js
@@ -1,21 +1,26 @@
+'use strict';
 
 function bot(x) {
     let number = prompt('Угадай число от 1 до 100');
     return function() {
-        if(+number > x) {
-            alert('Загаданное число меньше');
-            let start = bot(x);
-            start();
-        } else if (+number < x) {
-            alert('Загаданное число больше');
-            let start = bot(x);
-            start();
-        } else if(isNaN(+number)){
-            alert('Введи число!');
-            let start = bot(x);
-            start();
-        } else if (+number === x) {
-            alert('Верно');
+        if(number === null){
+            return;
+        }else {
+            if(+number > x) {
+                alert('Загаданное число меньше');
+                let start = bot(x);
+                start();
+            } else if (+number < x) {
+                alert('Загаданное число больше');
+                let start = bot(x);
+                start();
+            } else if(isNaN(+number)){
+                alert('Введи число!');
+                let start = bot(x);
+                start();
+            } else if (+number === x) {
+                alert('Верно');
+            }
         }
     }
 }
