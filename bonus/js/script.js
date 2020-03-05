55ce00b3fd96bbc3caec632a61440de332d015b0
Mariy <marodina87@gmail.com>
2020-03-06 00:23:40 +0300

    fix

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -19,75 +19,74 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             if(request.status === 200) {
                 responseData = JSON.parse(request.response);
-                console.log(responseData);
+                parseData(responseData.RU, 'default', 3);
             }
         });
     }
 
-    getCities(url);
-
-    console.log(responseData);
-
-
-
-    const setData = (data) => {
-        return data;
-    }
-
-    const parseDataDefault = (data) => {
-        const ruArr = data.RU;
-        ruArr.forEach(item => {
+    const parseData = (data, selector, countCountry) => {
+        console.log(data);
+        let dropdownCols = document.querySelector(`.dropdown-lists__list--${selector}`);
+        dropdownCols = dropdownCols.querySelector('.dropdown-lists__col');
+        dropdownCols.innerHTML = '';
+        data.forEach(item => {
             const countryBlock = document.createElement('div');
             countryBlock.classList.add('dropdown-lists__countryBlock');
             const totalLine =
-                `<div class="dropdown-lists__total-line">
-                    <div class="dropdown-lists__country" data-country="${item.country}">${item.country}</div>
+                `<div class="dropdown-lists__total-line" data-country="${item.country}">
+                    <div class="dropdown-lists__country">${item.country}</div>
                     <div class="dropdown-lists__count">${item.count}</div>
                  </div>`;
             countryBlock.insertAdjacentHTML('beforeend', totalLine);
 
             item.cities.forEach((city, ind) => {
-                if (ind > 2) {
-                    return;
+                if(countCountry) {
+                    if (ind >= countCountry) {
+                        return;
+                    }
                 }
                 const line =
-                `<div class="dropdown-lists__line" data-link="${city.link}">
+                    `<div class="dropdown-lists__line" data-link="${city.link}">
                     <div class="dropdown-lists__city">${city.name}</div>
                     <div class="dropdown-lists__count">${city.count}</div>
                  </div>`;
                 countryBlock.insertAdjacentHTML('beforeend', line);
             });
 
-            dropdownCols[0].insertAdjacentElement('beforeend', countryBlock);
+            dropdownCols.insertAdjacentElement('beforeend', countryBlock);
         })
-    };
-
-    const parseDataSelect = (data) => {
-
     }
 
-    const openDefaultList = () => {
-        defaultList.style.cssText = 'display: block';
-    }
-
-    const hiddenDefaultList = () => {
-        defaultList.style.cssText = 'display: none';
-    }
 
     selectCities.addEventListener('focus', (event) =>{
-        openDefaultList();
+        defaultList.style.cssText = 'display: block';
+        selectList.style.cssText = 'display: none';
     });
 
     document.addEventListener('click', (event) => {
         const target = event.target;
+
         if (!event.target.closest('.input-cities')) {
-            hiddenDefaultList();
+            defaultList.style.cssText = 'display: none';
         }
 
-        if (target.matches('.dropdown-lists__country')) {
-            const dataValue = target.dataset.country;
-            parseDataSelect(dataValue);
+        if (target.closest('.dropdown-lists__list--default') && target.closest('.dropdown-lists__total-line')) {
+            const dataValue = target.closest('.dropdown-lists__total-line').dataset.country;
+            let data = responseData.RU;
+            data = data.filter((item) => {
+                return item.country === dataValue;
+            });
+            parseData(data, 'select', false);
+            defaultList.style.cssText = 'display: none';
+            selectList.style.cssText = 'display: block';
+        }
+
+        if(target.closest('.dropdown-lists__list--select') && target.closest('.dropdown-lists__total-line')) {
+            defaultList.style.cssText = 'display: block';
+            selectList.style.cssText = 'display: none';
         }
     });
+
+    getCities(url);
 
 });
